6c978cc62c611e6196627b852b267ccd321bf3da
collion <charles.ollion@gmail.com>
2017-05-23 18:11:44 +0200

    add small test

--- test.py
+++ test.py
@@ -9,12 +9,23 @@ print("entity for boy")
 ent_name = get_entity("boy")
 print(ent_name)
 
-print("details for entity %%lu:01000061")
-ent_details = get_entity_details('%%lu:01000061', "24")
-print(ent_details)
-
 print("add a fake md5 to entity")
 entity = "%%lu:01000061"
 md5 = "234252f232"
 result = insert_image(entity, md5, action="add")
 print("result: ", result)
+
+print("details for entity %%lu:01000061")
+ent_details = get_entity_details('%%lu:01000061', "24")
+print(ent_details)
+
+print("remove the fake md5 to entity")
+entity = "%%lu:01000061"
+md5 = "234252f232"
+result = insert_image(entity, md5, action="remove")
+print("result: ", result)
+
+print("details for entity %%lu:01000061")
+ent_details = get_entity_details('%%lu:01000061', "24")
+print(ent_details)
+
