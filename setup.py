aa2b4a7acedefbdbb94e4e5b8c7b1c39e34fa095
skoos <skoos@heuritech.com>
2017-05-30 14:30:33 +0200

    Import/Creation of proper module arch

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='thesaurusutils',
+    version='0.1',
+    description='Python Wrapper for Thesaurus API',
+    url='http://github.com/skoos/thesaurus-utils',
+    package_dir={'':'src'},
+    packages=find_packages('src'),
+)
