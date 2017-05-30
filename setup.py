86e83c2d42418b4159af28595d2cf10cbd2b2b74
skoos <skoos@heuritech.com>
2017-05-30 14:40:15 +0200

    Adding requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='thesaurusutils',
-    version='0.1',
-    description='Python Wrapper for Thesaurus API',
-    url='http://github.com/skoos/thesaurus-utils',
-    package_dir={'':'src'},
-    packages=find_packages('src'),
-)
+with open('requirements.txt', 'r') as reqfile:
+    setup(
+        name='thesaurusutils',
+        version='0.1',
+        description='Python Wrapper for Thesaurus API',
+        url='http://github.com/skoos/thesaurus-utils',
+        package_dir={'':'src'},
+        packages=find_packages('src'),
+        install_requires=reqfile.readlines()
+    )
